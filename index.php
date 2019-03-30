d6eecc2b151979aa5c538a94b448fce7042593af
Cadiducho <cadiducho@gmail.com>
2019-03-30 19:48:21 +0100

    Corregir carga de la plantilla

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require_once 'vendor/autoload.php';
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader, [
     'cache' => 'compilation_cache',
+    'cache' => false,
 ]);
 
-echo $twig->render('portada.twig', ['web_title' => 'Mario Tennis Club']);
+echo $twig->render('portada.twig', []);
 
 ?>
